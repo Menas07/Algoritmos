acba55bad9246c6aae17d00c9c96680c4c768295
Menandro Andrade <menandroandrade18@gmail.com>
2024-01-12 10:47:50 -0300

    Finalização do linear search

--- linear_search.rb
+++ linear_search.rb
@@ -1,4 +1,19 @@
-def linear_search (array, target)
+def linear_search(array, target)
     array.each_with_index do |element, index|
         return index if element == target
     end
+    return -1
+end
+
+# exemplo de uso:
+
+arr = [2, 7, 10, 18, 22]
+target_value = 11
+result = linear_search(arr, target_value)
+if result != -1
+    puts "O elemento #{target_value} foi encontrado no índice #{result}"
+else
+    puts "O elemento #{target_value} não foi encontrado."
+end
+
+
